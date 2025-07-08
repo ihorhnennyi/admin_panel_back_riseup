f0b96721c28247d0b422060d5a21812868383ccf
ihorhnennyi <ihorhnennyi@gmail.com>
2025-07-08 07:53:16 +0300

    fix(cors): allow API origin and dev frontend

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
 		'http://localhost:5173',
 		'http://localhost:5000',
 		'http://workriseup.website',
+		'http://api.workriseup.website',
+		'https://api.workriseup.website',
 	]
 
 	app.enableCors({
